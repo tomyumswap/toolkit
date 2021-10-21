db7f0a626a43f99bf2dd2b21503da8239e5e0f38
Rushikesh <rushikesh.gadekar@cipherventurecapital.com>
2021-10-21 16:41:11 +0530

    fix(theming): Theming

--- packages/pancake-uikit/src/widgets/Menu/Menu.tsx
+++ packages/pancake-uikit/src/widgets/Menu/Menu.tsx
@@ -29,8 +29,8 @@ const StyledNav = styled.nav<{ showMenu: boolean }>`
   align-items: center;
   width: 100%;
   height: ${MENU_HEIGHT}px;
-  background-color: ${({ theme }) => theme.nav.background};
-  border-bottom: 1px solid ${({ theme }) => theme.colors.cardBorder};
+  background-color: transparent;
+  border-bottom: none;
   z-index: 20;
   transform: translate3d(0, 0, 0);
 
